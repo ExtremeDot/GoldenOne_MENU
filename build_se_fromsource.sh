77b93fb2c1d721679064825f93ccf08810b9b534
DisaterOne <120102306+ExtremeDot@users.noreply.github.com>
2023-01-02 17:36:44 +0330

    Update build_se_fromsource.sh

--- build_se_fromsource.sh
+++ build_se_fromsource.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# installing dnsmasq
+systemctl stop systemd-resolved && sleep 5
+sudo apt install dnsmasq
+sleep 5
+service dnsmasq restart
+sleep 5
+
 mkdir /se_install && cd /se_install
 sudo apt -y install build-essential net-tools cmake gcc g++ make rpm pkg-config libncurses5-dev libssl-dev libsodium-dev libreadline-dev zlib1g-dev
 
@@ -14,3 +21,19 @@ make -C build install
 
 echo "vpnserver start"
 echo "vpncmd"
+
+cat <<EOF > /etc/dnsmasq.conf
+interface=tap_soft
+dhcp-range=tap_soft,10.100.10.128,10.100.10.254,12h
+dhcp-option=tap_soft,3,10.100.10.1
+dhcp-option=option:dns-server,10.100.10.1,1.1.1.1
+bind-interfaces
+no-poll
+no-resolv
+bogus-priv
+server=1.1.1.1
+server=1.0.0.1
+
+EOF
+sleep 2
+service dnsmasq restart
