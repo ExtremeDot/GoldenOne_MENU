e78c2032c0f697b20c17515084518c5dc4fc4c31
DisaterOne <120102306+ExtremeDot@users.noreply.github.com>
2023-01-20 13:40:45 +0330

    Update MAIN.sh

--- MAIN.sh
+++ MAIN.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "G O L D E N    O N E     MENU      V1.7"
+echo "G O L D E N    O N E     MENU      V1.8"
 echo "----------------------------------------"
 PS3=" $(echo $'\n'-----------------------------$'\n' "   Enter Option: " ) "
 echo ""
